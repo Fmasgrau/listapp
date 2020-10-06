4588d7b3d7bb948c6a6cbffdd55f5da7f676c6aa
Facundo Gaston Masgrau <facundo.masgrau@entreconsultas.com>
2020-10-06 00:12:07 -0300

    Entity Id key option added

--- src/Components/EntityId/EntityId.jsx
+++ src/Components/EntityId/EntityId.jsx
@@ -5,9 +5,9 @@ export default function EntityId() {
     return (
         <React.Fragment>
             <select className="form-control" id="exampleFormControlSelect1">
-                {EntityIdJson.map(res => {
-                    console.log(res)
-                    return <option>{res.key}</option>
+                {EntityIdJson.map((res,index) => {
+                    
+                    return <option key={index}>{res.key}</option>
                 })}
             </select>
         </React.Fragment>
